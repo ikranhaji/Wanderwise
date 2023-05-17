b45085421230106c717a9382550a7ded89c07269
Gianni Thi <gianniyonnithi@gmail.com>
2023-05-17 22:14:40 +0000

    Resolve "As a user I can save the recommendation to my profile"

--- wanderwise/models/recommendations.py
+++ wanderwise/models/recommendations.py
@@ -3,3 +3,10 @@ from pydantic import BaseModel
 class RecommendationIn(BaseModel):
     location: str
     interest: str
+    account_id: str
+
+class RecommendationSaveIn(RecommendationIn):
+    recommendations: str
+
+class RecommendationSaveOut(RecommendationSaveIn):
+    id: str
---
+++ wanderwise/queries/recommendations.py
@@ -0,0 +1,62 @@
+from queries.client import MongoQueries
+from bson.objectid import ObjectId
+
+from models.recommendations import (
+    RecommendationSaveIn,
+    RecommendationSaveOut
+)
+
+
+class RecommendationQueries(MongoQueries):
+    collection_name = "recommendations"
+
+    def get(self, username: str) -> RecommendationSaveOut:
+        recommendations = []
+        for recommendation in self.collection.find({"username": username}):
+            recommendation["id"] = str(recommendation["_id"])
+            recommendations.append(RecommendationSaveOut(**recommendation))
+        return recommendations
+
+    # def get_one(self) -> RecommendationSaveOut:
+    #     pipeline = [
+    #         {'$match': {'location': 'NYC'}},
+    #         {
+    #             '$lookup': {
+    #                 'from': 'accounts',
+    #                 'localField': 'account_id',
+    #                 'foreignField': '_id',
+    #                 'as': 'account_id'
+    #             }
+    #         }, {
+    #             '$addFields': {
+    #                 'account_id': {
+    #                     '$map': {
+    #                         'input': '$account_id',
+    #                         'as': 'account',
+    #                         'in': {
+    #                             'username': '$$account.username',
+    #                             'name': '$$account.full_name',
+    #                             'id': '$$account._id'
+    #                         }
+    #                     }
+    #                 }
+    #             }
+    #         }
+    #     ]
+    #     response = self.collection.aggregate(pipeline)
+    #     for doc in response:
+    #         doc['id'] = str(doc["_id"])
+    #         doc["account_id"] = str(doc["account_id"])
+    #         return  RecommendationSaveOut(**doc)
+
+    def create(self, info: RecommendationSaveIn, account_id: str) -> RecommendationSaveOut:
+        recommendation = info.dict()
+        recommendation["account_id"] = ObjectId(account_id)
+        result = self.collection.insert_one(recommendation)
+        if result.inserted_id:
+            recommendation["id"] = str(result.inserted_id)
+        return RecommendationSaveOut(**recommendation)
+
+    def delete(self, recommendation_id: str):
+        results = self.collection.delete_one({"_id": ObjectId(recommendation_id)})
+        return results.deleted_count > 0
