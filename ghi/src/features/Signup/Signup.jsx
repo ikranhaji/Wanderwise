ef8e3c264cc98c9ee0f09ab21048b459e1b2e3c2
ikranhaji <ikranhaji4@gmail.com>
2023-05-30 14:19:00 -0500

    added password confirmation

--- ghi/src/features/Signup/Signup.jsx
+++ ghi/src/features/Signup/Signup.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import { useCreateAccountMutation, useGetTokenQuery } from '../../app/apiSlice';
+import { useCreateAccountMutation } from '../../app/apiSlice';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
 
 const Signup = () => {
 	const navigate = useNavigate();
 	const [create, result] = useCreateAccountMutation();
-    const { data: account } = useGetTokenQuery();
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
 	const [full_name, setFull_name] = useState('');
 	const handleSubmit = (e) => {
 		e.preventDefault();
-        create( { username, password, full_name } )
-		console.log("this is result===>",result);
-		navigate('/createrecommendations')
+		if (password === confirmPassword) {
+			create({ username, password, full_name })
+			navigate('/createrecommendations')
+		}
+		else {
+			alert("Passwords do not match")
+		}
 	};
 	return (
 		<div>
@@ -25,20 +29,27 @@ const Signup = () => {
 					value={username}
 					onChange={(e) => setUsername(e.target.value)}
 				/>
-				<label htmlFor="password"> Password </label>
-				<input
-                    type='password'
-					name="password"
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
 				<label htmlFor="fullname">First Name & Last Name</label>
 				<input
 					name="fullname"
 					value={full_name}
 					onChange={(e) => setFull_name(e.target.value)}
 				/>
-                <button type='submit'>Submit</button>
+				<label htmlFor="password"> Password </label>
+				<input
+					type='password'
+					name="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<label htmlFor="password"> Confirm Password </label>
+				<input
+					type='password'
+					name="confirm_password"
+					value={confirmPassword}
+					onChange={(e) => setConfirmPassword(e.target.value)}
+				/>
+				<button type='submit'>Submit</button>
 			</form>
 		</div>
 	);
