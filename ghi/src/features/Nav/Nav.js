15ede23e80537d3bc841a40f7e89d403370e986c
Paul <paulbaumann830@gmail.com>
2023-05-31 11:28:44 -0500

    format/css

--- ghi/src/features/Nav/Nav.js
+++ ghi/src/features/Nav/Nav.js
@@ -1,20 +1,30 @@
-import './Nav.css'
-import { NavLink } from 'react-router-dom'
-import { useLogoutMutation, useGetTokenQuery, useDeleteAccountMutation } from '../../app/apiSlice';
+import "./Nav.css";
+import { NavLink } from "react-router-dom";
+import {
+  useLogoutMutation,
+  useGetTokenQuery,
+  useDeleteAccountMutation,
+} from "../../app/apiSlice";
 
 export default function Nav() {
-		const [deleteAccount] = useDeleteAccountMutation()
-		const { data: account } = useGetTokenQuery();
-		const [logout] = useLogoutMutation()
+  const [deleteAccount] = useDeleteAccountMutation();
+  const { data: account } = useGetTokenQuery();
+  const [logout] = useLogoutMutation();
 
-	return (
-		<div className="container blue circleBehind">
-			<NavLink to='/'>HOME</NavLink>
-			{account && <NavLink to='/createrecommendations'>RECOMMENDATIONS</NavLink>}
-			{account && <NavLink to='/myprofile'>Profile</NavLink>}
-			{!account && <NavLink to='/auth/signup'>SIGNUP</NavLink>}
-			{!account &&<NavLink to='/auth/login'>LOGIN</NavLink>}
-			{account && <NavLink to='/' onClick={logout}>LOGOUT</NavLink>}
-		</div>
-	);
+  return (
+    <div className="container blue circleBehind">
+      <NavLink to="/">HOME</NavLink>
+      {account && (
+        <NavLink to="/createrecommendations">RECOMMENDATIONS</NavLink>
+      )}
+      {account && <NavLink to="/myprofile">Profile</NavLink>}
+      {!account && <NavLink to="/auth/signup">SIGNUP</NavLink>}
+      {!account && <NavLink to="/auth/login">LOGIN</NavLink>}
+      {account && (
+        <NavLink to="/" onClick={logout}>
+          LOGOUT
+        </NavLink>
+      )}
+    </div>
+  );
 }
