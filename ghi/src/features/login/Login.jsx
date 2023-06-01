ac1f6a4c8a3e8567406413b3bfa61b9f44a4679f
Paul Baumann <paulbaumann830@gmail.com>
2023-06-01 01:18:06 +0000

    Resolve "Formatting and CSS"

--- ghi/src/features/login/Login.jsx
+++ ghi/src/features/login/Login.jsx
@@ -1,67 +1,69 @@
 // import './Login.css';
-import { useLoginMutation, useGetTokenQuery } from '../../app/apiSlice';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useLoginMutation, useGetTokenQuery } from "../../app/apiSlice";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-	const { data: account } = useGetTokenQuery();
-	const [login, results] = useLoginMutation();
-	const [username, setUserName] = useState('');
-	const [password, setPassword] = useState('');
+  const { data: account } = useGetTokenQuery();
+  const [login, results] = useLoginMutation();
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-	const navigate = useNavigate();
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		login({ username, password });
-		navigate('/myprofile')
-	};
-	return (
-		<>
-			<div className="container">
-				<div className="row">
-					<div className="col s12 m6 offset-m3">
-						<form onSubmit={handleSubmit}>
-							<div className="row">
-								<div className="input-field col s12">
-									<input
-										name="username"
-										type="text"
-										placeholder="Username"
-										value={username}
-										onChange={(e) => {
-											setUserName(e.target.value);
-										}}
-										required
-									/>
-								</div>
-							</div>
-							<div className="row">
-								<div className="input-field col s12">
-									<input
-										type="password"
-										placeholder="Password"
-										value={password}
-										onChange={(e) => setPassword(e.target.value)}
-										required
-									/>
-								</div>
-							</div>
-							<div className="row">
-								<div className="col s12">
-									<button class="btn waves-effect waves-light light-blue lighten-1" type="submit" name="action">Login
-										<i class="material-icons right">send</i>
-									</button>
-								</div>
-							</div>
-						</form>
-
-
-
-					</div>
-				</div >
-			</div >
-		</>
-	);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login({ username, password });
+    navigate("/myprofile");
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col s12 m6 offset-m3">
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    name="username"
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col s12">
+                  <button
+                    class="btn waves-effect waves-light light-blue lighten-1"
+                    type="submit"
+                    name="action"
+                  >
+                    Login
+                    <i class="material-icons right">send</i>
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import "materialize-css/dist/css/materialize.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
