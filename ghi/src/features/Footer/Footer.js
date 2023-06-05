1568c40f672c7333b9446efa143dfc01c4905d18
Paul <paulbaumann830@gmail.com>
2023-06-05 18:14:45 -0500

    25-footer

---
+++ ghi/src/features/Footer/Footer.js
@@ -0,0 +1,48 @@
+import "./Footer.css";
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="page-footer indigo lighten-3">
+      <div className="container">
+        <div className="row">
+          <div className="col l6 s12">
+            <h5 className="white-text">WanderWise</h5>
+            <p className="grey-text text-lighten-4"></p>
+          </div>
+          <div className="col l4 offset-l2 s12">
+            <h5 className="white-text">Links</h5>
+            <ul>
+              <li>
+                <Link
+                  className="grey-text text-lighten-3"
+                  to="https://travel.state.gov/content/travel/en/traveladvisories/traveladvisories.html/"
+                >
+                  Travel Advisories
+                </Link>
+              </li>
+              <li>
+                <Link className="grey-text text-lighten-3" to="#!">
+                  Link 2
+                </Link>
+              </li>
+              <li>
+                <Link className="grey-text text-lighten-3" to="#!">
+                  Link 3
+                </Link>
+              </li>
+              <li>
+                <Link className="grey-text text-lighten-3" to="#!">
+                  Link 4
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <div className="container">© 2023 Team Jordan Year</div>
+      </div>
+    </footer>
+  );
+}
