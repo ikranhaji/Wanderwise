16693ae57c7f4ab617158ec661c727d7d8fb08ca
Ikran Haji <ikranhaji4@gmail.com>
2023-06-05 23:51:07 +0000

    fixed_nav"

--- ghi/src/features/Nav/Nav.js
+++ ghi/src/features/Nav/Nav.js
@@ -1,48 +1,99 @@
 import './Nav.css';
+
 import { NavLink } from 'react-router-dom';
 import {
-	useLogoutMutation,
-	useGetTokenQuery,
-	useDeleteAccountMutation,
+  useLogoutMutation,
+  useGetTokenQuery,
+  useDeleteAccountMutation,
 } from '../../app/apiSlice';
 import logo from '../Hero/logo.png';
-
-
-
-
+import { useEffect } from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js'
 
 export default function Nav() {
-	const [deleteAccount] = useDeleteAccountMutation();
-	const { data: account } = useGetTokenQuery();
-	const [logout] = useLogoutMutation();
+  const [deleteAccount] = useDeleteAccountMutation();
+  const { data: account } = useGetTokenQuery();
+  const [logout] = useLogoutMutation();
 
-	return (
+  useEffect(() => {
+    let sidenav = document.querySelector('.sidenav');
+    M.Sidenav.init(sidenav, {})
+  })
 
-		<div className="navbar-fixed">
-		  <nav>
-		      <div id = "navyy" className="teal lighten-3">
-		        <NavLink to="/">
-		          <img id="logo" src={logo} alt="Wander Wise" />
-		        </NavLink>
-		        <ul id="nav-mobile" className="right">
-		          <li>
-		            {account && (
-		              <NavLink to="/createrecommendations">RECOMMENDATIONS</NavLink>
-		            )}
-		          </li>
-		          <li>{account && <NavLink to="/myprofile">Profile</NavLink>}</li>
-		          <li>{!account && <NavLink to="/auth/signup">SIGNUP</NavLink>}</li>
-		          <li>{!account && <NavLink to="/auth/login">LOGIN</NavLink>}</li>
-		          <li>
-		            {account && (
-		              <NavLink to="/" onClick={logout}>
-		                LOGOUT
-		              </NavLink>
-		            )}
-		          </li>
-		        </ul>
-		      </div>
-		  </nav>
-		</div>
-	);
+
+  return (
+    <>
+      <nav>
+        <div className="nav-wrapper">
+          <NavLink to="/">
+            <img id="logo" src={logo} alt="Wander Wise" />
+          </NavLink>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <ul className="right hide-on-med-and-down">
+            <li>
+              {account && (
+                <NavLink to="/createrecommendations">RECOMMENDATIONS</NavLink>
+              )}
+            </li>
+            <li>{account && <NavLink to="/myprofile">Profile</NavLink>}</li>
+            <li>{!account && <NavLink to="/auth/signup">SIGNUP</NavLink>}</li>
+            <li>{!account && <NavLink to="/auth/login">LOGIN</NavLink>}</li>
+            <li>
+              {account && (
+                <NavLink to="/" onClick={logout}>
+                  LOGOUT
+                </NavLink>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul className="sidenav" id="mobile-demo">
+        <li>
+          {account && (
+            <NavLink to="/createrecommendations">RECOMMENDATIONS</NavLink>
+          )}
+        </li>
+        <li>{account && <NavLink to="/myprofile">Profile</NavLink>}</li>
+        <li>{!account && <NavLink to="/auth/signup">SIGNUP</NavLink>}</li>
+        <li>{!account && <NavLink to="/auth/login">LOGIN</NavLink>}</li>
+        <li>
+          {account && (
+            <NavLink to="/" onClick={logout}>
+              LOGOUT
+            </NavLink>
+          )}
+        </li>
+      </ul>
+
+    </>
+  )
 }
+
+    // <div className="navbar-fixed">
+    //   <nav>
+    //       <div id = "navyy" className="teal lighten-3">
+    //         <NavLink to="/">
+    //           <img id="logo" src={logo} alt="Wander Wise" />
+    //         </NavLink>
+    //         <ul id="nav-mobile" className="right">
+    //           <li>
+    //             {account && (
+    //               <NavLink to="/createrecommendations">RECOMMENDATIONS</NavLink>
+    //             )}
+    //           </li>
+    //           <li>{account && <NavLink to="/myprofile">Profile</NavLink>}</li>
+    //           <li>{!account && <NavLink to="/auth/signup">SIGNUP</NavLink>}</li>
+    //           <li>{!account && <NavLink to="/auth/login">LOGIN</NavLink>}</li>
+    //           <li>
+    //             {account && (
+    //               <NavLink to="/" onClick={logout}>
+    //                 LOGOUT
+    //               </NavLink>
+    //             )}
+    //           </li>
+    //         </ul>
+    //       </div>
+    //   </nav>
+    // </div>
